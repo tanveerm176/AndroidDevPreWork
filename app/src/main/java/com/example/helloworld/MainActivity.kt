4ba60aa7c6f5b0acea97853f01e581f1c7507d57
tanveerm176 <tanveerm176@gmail.com>
2023-01-15 14:34:00 -0500

    added functionality for button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.helloButton)
+
+        button.setOnClickListener {
+
+            Log.v("button", "button was clicked")
+            Toast.makeText(this,"Hello to you as well!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
